acca2026b41d35aa4379be8f52a2afa4e7416d13
Diogo <65966727+diogopsa@users.noreply.github.com>
2022-09-20 18:51:46 -0300

    Add files via upload

---
+++ ThisApplication.Designer.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: IF.Modelador
+ * Date: 20/09/2022
+ * Time: 17:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace diogo {
+
+    public sealed partial class ThisApplication : Autodesk.Revit.UI.Macros.ApplicationEntryPoint {
+        
+        public event System.EventHandler Startup;
+        
+        public event System.EventHandler Shutdown;
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        private void OnStartup() {
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void FinishInitialization() {
+            base.FinishInitialization();
+            this.OnStartup();
+            this.InternalStartup();
+            if ((this.Startup != null)) {
+                this.Startup(this, System.EventArgs.Empty);
+            }
+        }
+        
+        /// 
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override void OnShutdown() {
+            if ((this.Shutdown != null)) {
+                this.Shutdown(this, System.EventArgs.Empty);
+            }
+            base.OnShutdown();
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
+        protected override string PrimaryCookie {
+            get {
+                return "ThisApplication";
+            }
+        }
+    }
+}
---
+++ ThisApplication.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace diogo
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId("F1FC7E94-6873-4314-9CB8-506D63F7D6D1")]
+	public partial class ThisApplication
+	{
+		private void Module_Startup(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Module_Shutdown(object sender, EventArgs e)
+		{
+
+		}
+
+		#region Revit Macros generated code
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(Module_Startup);
+			this.Shutdown += new System.EventHandler(Module_Shutdown);
+		}
+		#endregion
+		public void teste()
+		{
+			var uidoc = this.ActiveUIDocument;
+			var doc = uidoc.Document;
+			var sel = uidoc.Selection;
+
+			var refParede = sel.PickObject(ObjectType.Element);
+			var parede = doc.GetElement(refParede);
+
+			var area_parametro = parede.get_Parameter(BuiltInParameter.HOST_AREA_COMPUTED);
+			var areaParede = area_parametro.AsDouble();
+			TaskDialog.Show("Área da parede selecionada", areaParede.ToString());
+		}
+	}
+}
